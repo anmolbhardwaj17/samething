6497ff6eb45217e3940a19d5651d3309bb980014
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-03-14 14:03:14 +0530

    nnew

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
      <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
+        {/* new path for about or cv */}
      </Routes>
      </div>
      
