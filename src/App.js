d398f2bf3b3f4e88bc92d7383fd0f449e59a6825
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-03-15 15:11:10 +0530

    new comments

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
      <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
-        {/* new path for abouT */}
+        {/* new path for aboutme */}
      </Routes>
      </div>
      
