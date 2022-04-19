489c69bb00d85328cb93594bb020bf357ec6bb71
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-04-19 15:16:16 +0530

    bankrupt clean

---
+++ src/components/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+<div className="aboutme">
+                <h1>About me</h1>
+                <p className="grey">Purpose driven web developer and designer with passion for user centric products and believe in balance between functionality and aesthetics in design.</p>
+            </div>
